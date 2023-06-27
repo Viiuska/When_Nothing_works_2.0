398f0b0284f8aaec5bfcbc9853944c610ce738f2
Viiuska <87257685+Viiuska@users.noreply.github.com>
2023-06-27 15:20:53 +0300

    User model and registartion

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+let UserSchema = new Schema({
+    name:{
+        type:String
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required: true
+    }
+});
+
+module.exports=mongoose.model('User', UserSchema)
--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,33 @@ const passport = require('passport')
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcryptjs');
 
+const User = require('../models/user')
+const config = require('../config/database');
+
 
 // Register
-router.get('register', (req, res, next)=>{
-    res.send('Register');
+router.post('register', (req, res, next)=>{
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+            if(err){res.json({success:false, msg:"Failed to register"})}
+            User.create({
+                name:req.body.name,
+                email:req.body.email,
+                username:req.body.username,
+                password:hash
+            }
+            ).then(result =>{
+                res.json({success:true, msg:"User registered"})
+            })
+        })
+
+    });
+
 });
 
 
 // Authenticate
-router.get('authentivate', (req, res, next)=>{
+router.post('authentivate', (req, res, next)=>{
     res.send('Authenticate');
 });
 
